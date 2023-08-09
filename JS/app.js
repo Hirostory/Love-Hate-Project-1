41fa98f35fa155be5824b49a16ffc8c446f5faf5
hirostory <hiroakisotooka@gmail.com>
2023-08-08 23:42:56 -0400

    added players

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,3 @@
-console.log(love)
 
 //gameboard 
 
@@ -10,11 +9,33 @@ const areaWidth = tile * columns
 const areaHeight = tile * columns
 
 //Love 
-const loveHeight = tile*2
-const loveWidth = tile + 8 
+const loveHeight = tile*3
+const loveWidth = tile + 22
+const loveX = tile * columns/5 
+const loveY = tile * columns - tile*2
 
-console.log(loveWidth)
-console.log(loveHeight)
+//Hate 
+const hateHeight = tile*3
+const hateWidth = tile +22
+const hateX = tile * columns/1.5
+const hateY = tile * columns - tile*2
+
+const players = [
+  {
+    name: "Love",
+    x: loveX,
+    y: loveY,
+    width: loveWidth,
+    height: loveHeight,
+  },
+  {
+    name: "Hate",
+    x: hateX,
+    y: hateY,
+    width: hateWidth,
+    height: hateHeight
+  }
+]
 
 
 window.onload = function() {
@@ -23,4 +44,18 @@ window.onload = function() {
       gameArea.height = areaHeight
     const context = gameArea.getContext("2d")
 
+
+    const loveImage = new Image()
+    loveImage.src = "Images/Love-Behind.png"
+    
+    loveImage.onload = () => {
+      context.drawImage(loveImage, players[0].x, players[0].y, players[0].height, players[0].width)
+    }
+    
+    const hateImage = new Image()
+    hateImage.src = "Images/Hate-back.png"
+
+    hateImage.onload = () => {
+      context.drawImage(hateImage, players[1].x, players[1].y, players[1].height, players[1].width)
+    }
 }
